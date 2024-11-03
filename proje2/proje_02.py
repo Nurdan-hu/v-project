43014611f4cb2fc74e5f9b4c103746c79660becf
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-11-03 13:15:13 +0300

    proje_2 seçim 2

--- proje2/proje_02.py
+++ proje2/proje_02.py
@@ -1,4 +1,3 @@
-import moduller_02.kişiler
 def telrehberi():
     print("╔═════════════════════╗")
     print("║  Telefon Rehberi    ║")
@@ -11,5 +10,21 @@ def telrehberi():
     print("╚═════════════════════╝")
     secim = input()
     if secim == "1":
-        moduller_02.kişiler.Kisi()
+        a= open(f"rehber.txt","r")
+        print(a)
+        okunan = a.read()
+        print(okunan)
         telrehberi()
+    elif secim == "2":
+        adsoyad = (input("Ad-Soyad Giriniz:"))
+        telno= (input("Telefon Numarasını Giriniz:"))
+
+        dosya = open("rehber.txt","a",encoding="utf-8")
+        dosya.write ( "\n" +adsoyad + "-" + telno )
+        print("Kayıt işlemi başarıyla gerçekleşmiştir.")
+    elif secim == "3":
+        pass
+    elif secim == "4":
+        pass
+telrehberi()
+input()
