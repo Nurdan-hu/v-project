bbe9ab4120e0e3007bbc29f534c98d5d76e7f91b
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-11-15 04:26:14 +0300

    proje2 çalışmaları

--- proje2/proje_02.py
+++ proje2/proje_02.py
@@ -1,3 +1,54 @@
+def kisi_listele():
+            kisiler = open(f"rehber.txt","r",encoding="utf-8")
+            for c, b in enumerate(kisiler):
+                  print(c,b)
+            
+            
+            
+def kisi_ekle():
+        adsoyad = (input("Ad-Soyad Giriniz:"))
+        telno= (input("Telefon Numarasını Giriniz:"))
+        dosya = open("rehber.txt","a",encoding="utf-8")
+        dosya.write (adsoyad + "-" + telno )
+        print("Kayıt işlemi başarıyla gerçekleşmiştir.")
+        
+    
+def rehber_sil():
+    print("Mevcut kayıtlar")
+    kisi_listele()
+    silinecek = int(input ("Hangi kayıt silinecek(numarasını girin):"))
+    dosya = open("rehber.txt","r+",encoding="utf-8")
+    okunan = dosya.readlines()
+    silinecekKayit = okunan[silinecek-1]
+    print("Silinecek kayıt : ", silinecekKayit)
+    dosya.close()
+
+    dosya = open("rehber.txt","w")    
+    for a in okunan:
+        if a != silinecekKayit: dosya.write(a)
+    dosya.close()  
+            
+def kisi_düzenleme():
+    print("Mevcut kayıtlar")
+    kisi_listele()
+    duzeltilecek = int(input ("Hangi kayıt duzeltilecek(numarasını girin):"))
+    dosya = open("rehber.txt","r+",encoding="utf-8")    
+    okunan = dosya.readlines()
+    # duzeltilecekKayit = okunan[duzeltilecek-1]
+    print("Düzeltilecek kayıt : ", duzeltilecekKayit)
+    dosya.close()
+
+    dosya = open("rehber.txt","w")    
+    for a in okunan:
+        if a == duzeltilecekKayit: 
+            print("\n\n Yeni bilgileri giriniz:")
+            ad  = input("Ad  : ")
+            tel = input("Tel : ")
+            dosya.write(f"{ad}-{tel}\n")
+        else: dosya.write(a)
+
+    dosya.close()
+
 def telrehberi():
     print("╔═════════════════════╗")
     print("║  Telefon Rehberi    ║")
@@ -9,27 +60,18 @@ def telrehberi():
     print("║    Seçimiz nedir?   ║")
     print("╚═════════════════════╝")
     secim = input()
-    if secim == "1":
-        a= open(f"rehber.txt","r")
-        print(a)
-        okunan = a.read()
-        print(okunan)
+
+    if secim == "1": 
+        kisi_listele()
         telrehberi()
-    elif secim == "2":
-        adsoyad = (input("Ad-Soyad Giriniz:"))
-        telno= (input("Telefon Numarasını Giriniz:"))
-        dosya = open("rehber.txt","a",encoding="utf-8")
-        dosya.write ( "\n" +adsoyad + "-" + telno )
-        print("Kayıt işlemi başarıyla gerçekleşmiştir.")
+    if secim == "2" : 
+        kisi_ekle()
         telrehberi()
-    elif secim == "3":
-        pass
-    elif secim == "4":
-        pass
-        # print("Düzenlemek istediğiniz ismi giriniz:")
-        # yeniisim=print("Yeni isimi giriniz:")
-        # yenino=print("Yeni numarayı giriniz:")
-        # dosya = open("rehber.txt","a",encoding="utf-8")
-        # dosya.write ( yeniisim + "-" + yenino )
+    if secim == "3" : 
+        rehber_sil()
+        telrehberi()
+    if secim == "4" : 
+        kisi_düzenleme()
+        telrehberi()
 telrehberi()
 input()
